9eada66421748cbb0774b458ef273fffa999bef6
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2022-08-01 14:39:46 +0100

    Updated styling of work section

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -27,32 +27,42 @@ export default function Work() {
   if (error) return "Error, please try something new";
 
   return (
-    <>
+    <div className="container">
       <div className="header">
         <h1 className="work">Our Work</h1>
         <p>We take pride in what we do and ensure our client comes first.</p>
       </div>
-      <div className="work-projects">
-        {data?.portfolios?.map((item) => (
-          <div key={item.slug}>
-            {" "}
-            Campaign:{" "}
-            <Link to={`/work/${item.slug}`}>
-              <a href={`work/${item.slug}`}>{item.title}</a>
-            </Link>
-            <div className="project-image">
-              <img
-                src={item.coverImage.url}
-                alt={`${item.title} campaign`}
-                className="project-img"
-              />
-            </div>
-            <p className="project-description">
-              {item.date}: {item.description}
-            </p>
-          </div>
-        ))}
+      <div className="projects__container">
+        <div className="projects__wrapper">
+          <ul className="projects__items">
+            {/* <div className="work-projects"> */}
+            {data?.portfolios?.map((item) => (
+              <li className="project-item" key={item.slug}>
+                {" "}
+                <p className="project-item-title">
+                  Campaign:{" "}
+                  <Link to={`/work/${item.slug}`}>
+                    <a className="project-item-link" href={`work/${item.slug}`}>
+                      {item.title}
+                    </a>
+                  </Link>
+                </p>
+                <div className="project__item__pic-wrap">
+                  <img
+                    src={item.coverImage.url}
+                    alt={`${item.title} campaign`}
+                    className="project__item__img"
+                  />
+                </div>
+                <p className="project__item__info">
+                  {item.date}: {item.description}
+                </p>
+              </li>
+            ))}
+            {/* </div> */}
+          </ul>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
